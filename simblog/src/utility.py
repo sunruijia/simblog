8c6ab9cc33f37651206663718be14b5e7d4a4882
sunruijia <sunruijia@4f503af4-01a0-11de-8259-6defae454439>
2009-06-24 04:54:41 +0000

    for email post blog function(parse email message)

--- simblog/src/utility.py
+++ simblog/src/utility.py
@@ -4,6 +4,7 @@ from functools import wraps
 from google.appengine.api import users
 from google.appengine.api import memcache
 from datetime import datetime
+import email
 
 def checkAdmin(method):
     @wraps(method)
@@ -35,3 +36,28 @@ def cache(method):
             memcache.set(key,(result,response.headers['last-modified']),1)
             return      
     return wrapper
+
+
+class Message:
+    def __init__(self,msg_raw):
+        msg = email.message_from_string(msg_raw)
+        self.data = {}
+        self.data["email-date"] = msg["Received"].split(";")[1].strip()
+        email_subject = email.Header.decode_header(msg["Subject"])
+        if not email_subject:
+            self.data["email-subject"] = "Untitled"
+        else:
+            self.data["email-subject"] = email_subject[0][0].decode(email_subject[0][1])
+        self.payload_data = self._get_payload_data(msg)
+        self.data["email-text"] = self.payload_data
+        
+    def _get_payload_data(self, msg):
+        if msg.is_multipart():
+            payloads = msg.get_payload()
+            return self._get_payload_data(payloads[0])
+        else:
+            payload_data = msg.get_payload(decode=True).decode(msg.get_param('charset'))  
+        return payload_data
+    
+    
+    
